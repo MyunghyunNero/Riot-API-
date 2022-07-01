a87ee4fa305c5b913122546688d003f7a580ab0a
MyunghyungNero <mh1kim0910@gmail.com>
2022-07-02 01:16:03 +0900

    use with riot-api

---
+++ Riot API (1)/src/main/java/Riot/repository/SummonerRepository.java
@@ -0,0 +1,8 @@
+package Riot.repository;
+
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SummonerRepository {
+}
